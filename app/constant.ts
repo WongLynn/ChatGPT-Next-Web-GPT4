15cd98cecd8024939bdb9f04751aa253064c39f4
AmerlinGPT hung <yalinwang2@gmail.com>
2023-05-01 13:06:58 +0000

    修改的constant 文件添加常量

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,8 @@
+// 这是constant 定义一些常量函数
 export const OWNER = "Yidadaa";
 export const REPO = "ChatGPT-Next-Web";
 export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const SHOP_URL = `https://shop.macthink.top/#/`; // 添加常量，定义的后续访问的购买的链接
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
